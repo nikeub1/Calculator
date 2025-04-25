660b3a6da82a7e24d3afdfcd74b23ea24f3e2836
nikeub1 <nikeuprof@gmail.com>
2025-04-25 17:49:31 +0400

    Nikoloz's-Work
    Business Logic for the history of the calculations and ect.

---
+++ Calculator/Calculator/Services/ICalculationHistoryService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Calculator.Models;
+
+namespace Calculator.Services
+{
+    public interface ICalculationHistoryService
+    {
+        void Add(CalculationRecord record);
+        IEnumerable<CalculationRecord> GetAll();
+    }
+}
+
